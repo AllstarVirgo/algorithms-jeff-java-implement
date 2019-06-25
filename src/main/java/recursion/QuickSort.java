ddbc7b6115074cc7ff437684d48a9bc61c7a9710
王永兴 <yongxingwang@sf-express.com>
2019-06-25 08:37:30 +0800

    Quick Select Implement

--- src/main/java/recursion/QuickSort.java
+++ src/main/java/recursion/QuickSort.java
@@ -5,7 +5,34 @@ package recursion;
  */
 public class QuickSort {
     public void quickSort(int[] array) {
-        quickSortHelper(array,0,array.length-1);
+        quickSortHelper(array, 0, array.length - 1);
+    }
+
+    /**
+     * Get the k th smallest number in array
+     *
+     * @param array
+     * @param k
+     * @return
+     */
+    public int quickSlect(int[] array, int k) {
+        return quickSelectHelper(array, 0, array.length - 1, k);
+    }
+
+    private int quickSelectHelper(int[] array, int lo, int hi, int k) {
+        int len = hi-lo+1;
+        if(len==1)
+            return array[lo];
+        else {
+            int pivotIndex = lo;
+            int p = partion(array, pivotIndex, lo, hi);
+            if(k<p){
+                return quickSelectHelper(array, lo, p-1, k);
+            }else if(k>p){
+                return quickSelectHelper(array, p+1, hi, k);
+            }else
+                return array[k];
+        }
     }
 
     private void quickSortHelper(int[] array, int lo, int hi) {
@@ -28,7 +55,7 @@ public class QuickSort {
          *   array[0...n-2] compare to array[pivot]
          */
         int l = lo;
-        for (int i = lo; i < hi ; i++) {
+        for (int i = lo; i < hi; i++) {
             if (array[i] < array[hi]) {
                 swap(array, i, l);
                 l++;
--- src/test/java/recursion/QuickSortTest.java
+++ src/test/java/recursion/QuickSortTest.java
@@ -27,4 +27,22 @@ class QuickSortTest {
             assertArrayEquals(test,copy);
         }
     }
+
+    @Test
+    void quickSelect(){
+        for (int i = 1; i < 100; i++) {
+            QuickSort quickSort = new QuickSort();
+            int[] test = new int[i];
+            for (int j = 0; j < i; j++) {
+                test[j] = new Random().nextInt();
+            }
+            int[] copy = new int[i];
+            System.arraycopy(test,0,copy,0,i);
+            int index = new Random().nextInt(i);
+            Arrays.sort(copy);
+            int actual = quickSort.quickSlect(test,index);
+            int expect = copy[index];
+            assertEquals(expect,actual);
+        }
+    }
 }
