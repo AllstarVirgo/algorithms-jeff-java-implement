14dc0901f9de3dfa6e5d0bf97240d0da2e4b7526
01377809 <yongxingwang@sfmail.sf-express.com>
2019-12-09 10:00:22 +0800

    sort colors unresolved

---
+++ src/main/java/array/SortColors.java
@@ -0,0 +1,39 @@
+package array;
+
+/**
+ * @author potato
+ * @Date 2019/12/9 8:51
+ */
+public class SortColors {
+    /**
+     * Input: [2,0,2,1,1,0]
+     * Output: [0,0,1,1,2,2]
+     *It is hard to define what is a "one-pass" solution but this algorithm is bounded by O(2n),
+     *  meaning that at most each element will be seen and operated twice (in the case of all 0s).
+     *  You may be able to write an algorithm which goes through the list only once, but each step
+     *  requires multiple operations, leading the total operations larger than O(2n).
+     * @param nums
+     */
+    public void sortColors(int[] nums) {
+        if (nums.length == 0) return;
+        int p0 = 0, p2 = nums.length - 1;
+        int i = 0;
+        while (i <= p2) {
+            while (nums[i] == 2) {
+                exchange(nums, p2, i);
+                p2--;
+            }
+            while (nums[i] == 0) {
+                exchange(nums, p0, i);
+                p0++;
+            }
+            i++;
+        }
+    }
+
+    public void exchange(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
