83ea605d2ca4c47337970297350bc40071bd862e
01377809 <yongxingwang@sfmail.sf-express.com>
2019-12-10 09:08:24 +0800

    sort colors unresolved

--- src/main/java/array/SortColors.java
+++ src/main/java/array/SortColors.java
@@ -8,22 +8,24 @@ public class SortColors {
     /**
      * Input: [2,0,2,1,1,0]
      * Output: [0,0,1,1,2,2]
-     *It is hard to define what is a "one-pass" solution but this algorithm is bounded by O(2n),
-     *  meaning that at most each element will be seen and operated twice (in the case of all 0s).
-     *  You may be able to write an algorithm which goes through the list only once, but each step
-     *  requires multiple operations, leading the total operations larger than O(2n).
+     * It is hard to define what is a "one-pass" solution but this algorithm is bounded by O(2n),
+     * meaning that at most each element will be seen and operated twice (in the case of all 0s).
+     * You may be able to write an algorithm which goes through the list only once, but each step
+     * requires multiple operations, leading the total operations larger than O(2n).
+     *
      * @param nums
      */
     public void sortColors(int[] nums) {
         if (nums.length == 0) return;
         int p0 = 0, p2 = nums.length - 1;
         int i = 0;
-        while (i <= p2) {
-            while (nums[i] == 2) {
+        while (i <= p2 && p0 <= p2) {
+            while (nums[i] == 2 && p0 <= p2) {
                 exchange(nums, p2, i);
                 p2--;
             }
-            while (nums[i] == 0) {
+            i++;
+            while (nums[i] == 0 && p0 <= p2) {
                 exchange(nums, p0, i);
                 p0++;
             }
@@ -35,5 +37,14 @@ public class SortColors {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+    /**
+     * [2,1,1,0,0,2]
+     */
+
+    public static void main(String[] args) {
+        SortColors sortColors = new SortColors();
+        int[] nums = new int[]{2,0,2,1,1,0};
+        sortColors.sortColors(nums);
     }
 }
